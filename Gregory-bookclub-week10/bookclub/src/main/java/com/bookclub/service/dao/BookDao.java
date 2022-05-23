1e9f3ee6033c6d28098249c2f0713163ff491185
Kylie Gregory <80074900+kgregory413@users.noreply.github.com>
2022-05-22 20:19:26 -0400

    Add files via upload

---
+++ Gregory-bookclub-week10/bookclub/src/main/java/com/bookclub/service/dao/BookDao.java
@@ -0,0 +1,11 @@
+// This code was written as part of the bookclub application project
+// to fulfill the requirements of CIS530 at Bellevue University
+// Kylie Gregory - 5/22/2022
+
+package com.bookclub.service.dao;
+
+import com.bookclub.model.Book;
+import com.bookclub.service.GenericDao;
+
+public interface BookDao extends GenericDao<Book, String> {
+}
